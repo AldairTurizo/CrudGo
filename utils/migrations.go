50fe0b5b0e9f5a37a26694d2aa2fc91c7cff59d7
AldairTurizo <58045288+AldairTurizo@users.noreply.github.com>
2021-09-22 19:36:01 -0500

    formater

--- utils/migrations.go
+++ utils/migrations.go
@@ -1,13 +1,16 @@
 package utils
+
 import (
-"fmt"
-"github.com/chejo343/go_contacts/models"
+	"fmt"
+
+	"github.com/chejo343/go_contacts/models"
 )
+
 // MigrateDB migra la base de datos
 func MigrateDB() {
-db := GetConnection()
-defer db.Close()
-fmt.Println("Migrating models....")
-// Automigrate se encarga de migrar la base de datos sí no se ha migrado, y lo hace a partir del modelo
-db.AutoMigrate(&models.Contact{})
+	db := GetConnection()
+	defer db.Close()
+	fmt.Println("Migrating models....")
+	// Automigrate se encarga de migrar la base de datos sí no se ha migrado, y lo hace a partir del modelo
+	db.AutoMigrate(&models.Contact{})
 }
--- utils/response.go
+++ utils/response.go
@@ -1,15 +1,18 @@
 package utils
+
 import "net/http"
+
 // SendResponse envia datos json al cliente
 func SendResponse(w http.ResponseWriter, status int, data []byte) {
-w.Header().Set("Content-Type", "application/json")
-w.WriteHeader(status)
-w.Write(data)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
 }
+
 // SendErr envia un error al cliente, con contenido vacio
 func SendErr(w http.ResponseWriter, status int) {
-data := []byte(`{}`)
-w.Header().Set("Content-Type", "application/json")
-w.WriteHeader(status)
-w.Write(data)
+	data := []byte(`{}`)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
 }
